249bd082183e4f2e9b8b25d3c2ef2c276f960fe2
Don Alvin <alvindon41@gmail.com>
2023-09-19 21:45:19 +0300

    Set up routing

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,19 @@ import React from 'react'
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import Dashboard from './Pages/Dashboard'
 import LandingPage from './Pages/LandingPage'
+import Login from './Features/Auth/Login'
+import Register from './Features/Auth/Register'
+import ErrorPage from './Pages/ErrorPage'
 
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route>
       <Route path='/' element={<LandingPage />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/register' element={<Register />} />
       <Route path='/dashboard' element={<Dashboard />} />
+      <Route path='*' element={<ErrorPage />} />
     </Route>
   )
 )
